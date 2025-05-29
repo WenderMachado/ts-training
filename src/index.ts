c7bc3807ba3dc63249ca2a750d065f60e807ea88
Wender Machado <devwendermachado@gmail.com>
2025-05-29 13:43:24 -0300

    adiciona função para procurar membro adiciona função de aicionar membro

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,35 @@
-let spaceGarage = []
+type Spaceship = {
+  name: string
+  pilot: string
+  crewLimit: number
+  crew: string[]
+  inMission: boolean
+}
 
-function saveSpaceship(name: string, pilot: string, crewLimit: number){
- 
-  const newNave = {
+const spaceships: Spaceship[] = []
+
+function addSpaceship(name: string, pilot: string, crewLimit: number) {
+  const spaceship: Spaceship = {
     name,
     pilot,
     crewLimit,
     crew: [],
     inMission: false
   }
- 
-  spaceGarage.push(newNave)
-  alert(`a nave ${newNave.name} foi enviada`)
 
-
-
+  spaceships.push(spaceship)
+  alert(`A nave ${spaceship.name} foi registrada.`)
 }
 
-
-function sendSpaceship(name:string, captain:string) {
-  const spaceship = {
-    name, 
-    captain,
-    speed: 20,
-    inMission: true,
-    crew: []
-  }
-  alert(`a nave ${spaceship.name} comandada pelo capitão ${spaceship.captain} foi evidada para uma missao`) 
-  return spaceship
+function findSpaceship(name: string): Spaceship | undefined {
+  return spaceships.find((ship) => ship.name === name)
 }
 
-function accelerate(targetSpeed: number, spaceship:{name: string, speed: number}){
-  if(spaceship.speed > targetSpeed){
-    alert(`Reduzindo a velocidade da ${spaceship.name} para ${targetSpeed}`)
-  }else if (spaceship.speed < targetSpeed){
-    alert(`Aumentando a velocidade da ${spaceship.name} para ${targetSpeed}`)
-  }else{
-    alert(`mantenado a velocidade da ${spaceship.name}`)
+function addCrewMember(member: string, spaceship: Spaceship) {
+  if (spaceship.crew.length >= spaceship.crewLimit) {
+    alert(`${member} não pode ser adicionado à tripulação. Limite atingido.`)
+  } else {
+    spaceship.crew.push(member)
+    alert(`${member} foi adicionado à tripulação da ${spaceship.name}`)
   }
 }
-
-const spaceshipName = prompt(`insira o nome da nave a ser enviada`) || ""
-const spaceshipCaptain = prompt(`insira o nome do capitão da nave`) || ""
-if (spaceshipName === null || spaceshipCaptain === null) {
-  alert("Operação cancelada! Nome da nave ou capitão não inseridos.")
-} 
-
-const currentSpaceship = sendSpaceship(spaceshipName, spaceshipCaptain);
-const speed = parseFloat(prompt(`insira a velocidade a qual deseja acelerar`) || "")
-accelerate(speed, currentSpaceship)
-
-console.log(`logo`)
