a0df1b3360989f18af2e407199dea7567b111056
Wender Machado <devwendermachado@gmail.com>
2025-05-29 09:17:48 -0300

    refatora treio de ts, adicionando função de add spaceNave para declarar tipos

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
+let spaceGarage = []
+
+function saveSpaceship(name: string, pilot: string, crewLimit: number){
+ 
+  const newNave = {
+    name,
+    pilot,
+    crewLimit,
+    crew: [],
+    inMission: false
+  }
+ 
+  spaceGarage.push(newNave)
+  alert(`a nave ${newNave.name} foi enviada`)
+
+
+
+}
+
+
 function sendSpaceship(name:string, captain:string) {
   const spaceship = {
     name, 
@@ -20,11 +40,14 @@ function accelerate(targetSpeed: number, spaceship:{name: string, speed: number}
   }
 }
 
-const spaceshipName = prompt(`insira o nome da nave a ser enviada`)
-const spaceshipCaptain = prompt(`insira o nome do capitão da nave`)
+const spaceshipName = prompt(`insira o nome da nave a ser enviada`) || ""
+const spaceshipCaptain = prompt(`insira o nome do capitão da nave`) || ""
 if (spaceshipName === null || spaceshipCaptain === null) {
-  alert("Operação cancelada! Nome da nave ou capitão não inseridos.");
-} else {
-  const currentSpaceship = sendSpaceship(spaceshipName, spaceshipCaptain);
-  accelerate(30, currentSpaceship)
-}
+  alert("Operação cancelada! Nome da nave ou capitão não inseridos.")
+} 
+
+const currentSpaceship = sendSpaceship(spaceshipName, spaceshipCaptain);
+const speed = parseFloat(prompt(`insira a velocidade a qual deseja acelerar`) || "")
+accelerate(speed, currentSpaceship)
+
+console.log(`logo`)
