916084f38bc7ee07b48dc7117ce9c3f2f48e656b
Wender Machado <devwendermachado@gmail.com>
2025-05-29 13:59:48 -0300

    adiciona funções para 3 opções de menu

--- src/index.ts
+++ src/index.ts
@@ -44,3 +44,44 @@ function sendInMission(spaceship: {name: string, crewLimit: number, crew: string
     alert(`${spaceship.name} foi enviada`)
   }
 }
+
+function firstMenuOption(){
+  const name = prompt (`qual o nome da nave a ser registrada?`) || ""
+  const pilot = prompt (`qual o nome do piloto da ${name}`) || " "
+  const crewLimit = Number(prompt (`suporta quantas cabeça?`))
+
+  const confirmation = confirm (`quer adicionar a nave: ${name} com o piloto  ${pilot} com esse limite de tripulantes: ${crewLimit}`)
+
+  if(confirmation){
+    addSpaceship(name, pilot, crewLimit)
+  }
+}
+
+function secondMenuOption() {
+  const member = prompt('Qual é o nome do tripulante?') || ""
+  const spaceshipName = prompt(`Para qual nave ${member} deverá ser designado?`) || ""
+
+  const spaceship = findSpaceship(spaceshipName)
+
+  if (spaceship) {
+    const confirmation = confirm(`Confirma a inclusão de ${member} na tripulação da ${spaceship.name}?`)
+
+    if (confirmation) {
+      addCrewMember(member, spaceship)
+    }
+  }
+}
+
+function thirdMenuOption() {
+  const spaceshipName = prompt('Qual é o nome da nave a ser enviada?') || ""
+
+  const spaceship = findSpaceship(spaceshipName);
+
+  if (spaceship) {
+    const confirmation = confirm(`Confirma e envio da ${spaceship.name} na missão?`)
+
+    if (confirmation) {
+      sendInMission(spaceship)
+    }
+  }
+}
